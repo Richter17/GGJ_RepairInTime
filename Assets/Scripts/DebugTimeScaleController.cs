493654fc07777ecb58cebab65a7e8466f53853ee
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-01-30 23:49:57 +0200

    First logic
    Time scaled controllable
    Debug script
    Sample scene

---
+++ Assets/Scripts/DebugTimeScaleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTimeScaleController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 1)]
+    private float m_timeScale = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Cursor.visible = !Cursor.visible;
+        }
+
+        //Time.timeScale = m_timeScale;
+        //Time.fixedDeltaTime = m_timeScale * 0.02f;
+    }
+}
---
+++ Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DraggableObject : MonoBehaviour
+{
+    public float TimeMul = 1;
+    public float speedMul = 1;
+    private Rigidbody2D m_rigid;
+    private Vector3 m_mouseLastPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigid = GetComponent<Rigidbody2D>();
+        SetTimeScale(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        m_mouseLastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mouseRelative = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float delta = Vector3.Distance(mouseRelative, m_mouseLastPosition) * TimeMul;
+        m_rigid.velocity = (mouseRelative - transform.position) * speedMul;
+        SetTimeScale(delta);
+        //Debug.Log(m_rigid.velocity);
+        //Debug.Log(delta);
+        m_mouseLastPosition = mouseRelative;
+    }
+
+    private void OnMouseUp()
+    {
+        SetTimeScale(0);
+    }
+
+    private void SetTimeScale(float scale)
+    {
+        Time.timeScale = Mathf.Clamp(scale, 0, 1);
+        Time.fixedDeltaTime = scale * 0.02f;
+    }
+}
