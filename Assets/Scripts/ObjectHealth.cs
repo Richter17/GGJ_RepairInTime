d91c40231ac8510d0d874ae936215a39784a4f29
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-01-31 12:07:03 +0200

    Fixed Time Scale control logic
    Added RepairObject (main object)
    Fixed Inteface Time Controlker
    Removed TimeSettings

---
+++ Assets/Scripts/ObjectHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHealth : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_health = 100;
+    [SerializeField]
+    protected float m_damageMultiplier;
+    public float DamageMultiplier
+    {
+        get { return m_damageMultiplier; }
+    }
+    protected float m_remainingHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_remainingHealth = m_health;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ObjectHealth otherObj = other.gameObject.GetComponentInChildren<ObjectHealth>();
+        float damage = other.relativeVelocity.magnitude;
+        m_remainingHealth -= damage  * (otherObj ? otherObj.DamageMultiplier : 1);
+
+        if (m_health <= 0)
+            DestroyObject();
+        else
+            CrackEffect(m_remainingHealth / m_health);
+    }
+
+    protected virtual void DestroyObject()
+    {
+        Debug.Log("Destroy " + gameObject.name);
+    }
+
+    protected virtual void CrackEffect(float healthRatio)
+    {
+        Debug.Log("Crack " + gameObject.name);
+    }
+}
--- Assets/Scripts/TimeScaleController.cs
+++ Assets/Scripts/TimeScaleController.cs
@@ -2,19 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class TimeScaleController : MonoBehaviour
+namespace GGJ
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public class TimeScaleController : MonoBehaviour, ITimeController
     {
-        
+        private static TimeScaleController s_instance;
+        public static ITimeController GetTimeController()
+        {
+            if(s_instance == null)
+            {
+                GameObject go = new GameObject("TimeScaleController");
+                s_instance = go.AddComponent<TimeScaleController>();
+            }
+            return s_instance;
+        }
+
+        [SerializeField]
+        [Range(0, 1)]
+        private float m_minTimeScale = 0.05f, m_maxTimeScale = 0.85f;
+
+        private float m_prevoiusTimeScale;
+
+        public float TimeScale { get; set; }
+
+        public float MinTimeScale
+        {
+            get
+            {
+                return m_minTimeScale;
+            }
+        }
+
+        public float MaxTimeScale
+        {
+            get { return m_maxTimeScale; }
+        }
+
+        private bool m_isTimeControlled = false;
+        public bool IsTimeControlled
+        {
+            get { return m_isTimeControlled; }
+        }
+
+        private bool m_timePaused;
+        public bool TimePaused
+        {
+            get { return m_timePaused; }
+        }
+
+        public event ControlTimeStartedHandler TimeInputStarted;
+        public event ControlTimeFinishedHandler TimeInputFinished;
+        public event UpdateControlHandler ControlTimeUpdate;
+
+
+        void Start()
+        {
+            StartTimeScaleControl(0);
+        }
+
+        public void PauseTime(bool active)
+        {
+            m_timePaused = active;
+            if (active)
+            {
+                m_prevoiusTimeScale = TimeScale;
+                Time.timeScale = 0;
+                Time.fixedDeltaTime = 0;
+                TimeScale = 0;
+            }
+            else
+            {
+                StartTimeScaleControl(m_prevoiusTimeScale);
+            }
+        }
+
+        public void StartTimeScaleControl(float normalizedValue)
+        {
+            SetTimeScale(normalizedValue);
+            m_isTimeControlled = true;
+            TimeInputStarted?.Invoke();
+        }
+
+        public void EndTimeScaleControl()
+        {
+            StartTimeScaleControl(0);
+            m_isTimeControlled = false;
+            TimeInputFinished?.Invoke();
+        }
+
+        private void SetTimeScale(float val)
+        {
+            Time.timeScale = Mathf.Lerp(MinTimeScale, MaxTimeScale, val);
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            TimeScale = val;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 }
