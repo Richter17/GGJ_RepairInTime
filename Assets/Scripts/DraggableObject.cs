4574fbd356ba40c5b0eccba491f1e6e3091e837f
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-01-31 10:25:43 +0200

    Unity Ver update
    Updated to 2019.2.19'
    Added interface
    tweaked player

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -6,12 +6,21 @@ public class DraggableObject : MonoBehaviour
 {
     public float TimeMul = 1;
     public float speedMul = 1;
+    public float maxSpeed = 15;
+
+    public float minTimeScale = 0.1f;
+    public float maxTimeScale = 0.85f;
+
     private Rigidbody2D m_rigid;
     private Vector3 m_mouseLastPosition;
+    private Vector3 m_dragDelta;
+    private SpriteRenderer m_arrow;
+    private float m_arrowRatio;
     // Start is called before the first frame update
     void Start()
     {
         m_rigid = GetComponent<Rigidbody2D>();
+        m_arrow = transform.Find("Arrow")?.GetComponent<SpriteRenderer>();
         SetTimeScale(0);
     }
 
@@ -20,6 +29,20 @@ public class DraggableObject : MonoBehaviour
     {
 
     }
+
+    private void FixedUpdate()
+    {
+        m_rigid.AddForce(m_dragDelta);
+        Debug.Log(m_rigid.velocity.magnitude);
+        m_rigid.velocity = Vector3.ClampMagnitude(m_rigid.velocity, maxSpeed);
+        Vector2 arrowSize = m_arrow.size;
+        arrowSize.x = Mathf.Lerp(2.56f, 8f, m_rigid.velocity.magnitude / maxSpeed);
+        m_arrow.size = arrowSize;
+        float angle = Vector3.Angle(Vector3.right, m_rigid.velocity.normalized);
+        m_arrow.transform.localEulerAngles = new Vector3(0, 0, m_rigid.velocity.y > 0 ? angle: -angle);
+        //m_arrow.transform.localRotation = Quaternion.Euler(m_rigid.velocity.normalized);
+    }
+
 
     private void OnMouseDown()
     {
@@ -30,7 +53,7 @@ public class DraggableObject : MonoBehaviour
     {
         Vector3 mouseRelative = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float delta = Vector3.Distance(mouseRelative, m_mouseLastPosition) * TimeMul;
-        m_rigid.velocity = (mouseRelative - transform.position) * speedMul;
+        m_dragDelta = (mouseRelative - transform.position) * speedMul;
         SetTimeScale(delta);
         //Debug.Log(m_rigid.velocity);
         //Debug.Log(delta);
@@ -44,7 +67,7 @@ public class DraggableObject : MonoBehaviour
 
     private void SetTimeScale(float scale)
     {
-        Time.timeScale = Mathf.Clamp(scale, 0, 1);
-        Time.fixedDeltaTime = scale * 0.02f;
+        Time.timeScale = Mathf.Lerp(minTimeScale, maxTimeScale, scale);
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
     }
 }
---
+++ Assets/Scripts/ITimeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ
+{
+    public delegate void ControlTimeStartedHandler();
+    public delegate void ControlTimeFinishedHandler();
+    public delegate void UpdateControlHandler(float timeScale);
+    public interface ITimeController
+    {
+        float TimeScale { get; set; }
+        float MinTimeScale { get; }
+        float MaxTimeScale { get; }
+
+        bool IsTimeControlled { get; }
+
+        event ControlTimeStartedHandler ControlTimeStarted;
+        event ControlTimeFinishedHandler ControlTimeFinished;
+        event UpdateControlHandler ControlTimeUpdate;
+    }
+}
