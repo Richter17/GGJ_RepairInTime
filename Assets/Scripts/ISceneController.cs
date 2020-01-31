900bb69871a65879cb4d501ad4071a878d7e4e5a
IlyaMez <lianmez@gmail.com>
2020-01-31 12:11:21 +0200

    Add Main functionality and SceneController Interface

---
+++ Assets/Scripts/ISceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public delegate void LevelWinHandler();
+    public delegate void LevelFailHandler();
+    public delegate void PauseHandler();
+
+public interface ISceneController
+{
+    event LevelWinHandler LevelWon;
+    event LevelFailHandler LevelFailed;
+    event PauseHandler PauseRequest;
+
+    void Init();
+}
