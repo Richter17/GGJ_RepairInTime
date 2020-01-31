4910ecb0bacdbb0e06b475709be23b58bdfeba0f
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-01-31 16:29:49 +0200

    Added Mixer Interface

---
+++ Assets/Scripts/IMixerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+namespace GGJ.Audio
+{
+    public interface IMixerController
+    {
+        AudioMixer Master { get; }
+
+        void ResetMixer();
+        void ControlMixer(MixerArgs arg);
+        void ControlMixer(MixerArgs[] args);
+        void ClearParamters(string paramName);
+        void ClearParamters(string[] paramsNames);
+        void Mute(bool active);
+
+    }
+
+    public class MixerArgs
+    {
+        public string Name;
+        public float Value;
+        public MixerArgs(string name, float val)
+        {
+            Name = name;
+            Value = val;
+        }
+    }
+}
