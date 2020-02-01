75c882ae12373ac53e01d5a0f5df697eee1d24a8
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-02-01 13:43:16 +0200

    Hit Effect and added levels ssprites

---
+++ Assets/Scripts/EffectDestroySelf.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDestroySelf : MonoBehaviour
+{
+    private ParticleSystem m_ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_ps.isPlaying)
+            Destroy(gameObject);
+    }
+}
